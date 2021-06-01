5bf2f17a23f337f15ea42f8d2b1b614af7a0adfa
elboj <adaja.tobi1@gmail.com>
2021-06-01 15:36:44 +0100

    changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 /* eslint-disable */
 import React, { useState, useEffect } from "react";
-import Sidebar from "./components/Sidebar/Sidebar";
+// import Sidebar from "./components/Sidebar/Sidebar";
+import { Sidebar, SearchBar } from "./components/index";
 import "./index.css";
 
 function App() {
+  const [hidden, setHidden] = useState(true);
   return (
     <main>
       <section className="container">
-        <Sidebar />
+        {hidden && <Sidebar />}
+        <SearchBar />
       </section>
     </main>
   );
